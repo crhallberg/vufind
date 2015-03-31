88979e90e98ed98edf5a40ba9883a161656e9a80
Chris Hallberg <crhallberg@gmail.com>
2015-03-31 14:30:05 -0400

    Add telestrap theme.

---
+++ themes/telestrap/theme.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'extends' => 'bootstrap3',
+    'css' => array(
+        'telestrap.css',
+    ),
+    'less' => array(
+        'active' => true,
+        'compiled.less'
+    ),
+);
