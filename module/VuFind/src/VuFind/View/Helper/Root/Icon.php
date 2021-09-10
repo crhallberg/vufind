ac945fcc1fad179a86a437eb824955e8843cf2c0
Chris Hallberg <chris.hallberg@hey.com>
2021-09-10 14:43:53 -0400

    Fix MAJOR copy-paste error with Icon.php.

--- module/VuFind/src/VuFind/View/Helper/Root/Icon.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Icon.php
@@ -208,9 +208,6 @@ class Icon extends AbstractHelper
         if ($cached == null) {
             [$icon, $set, $template] = $this->mapIcon($name);
 
-            // Compile additional HTML attributes
-            $attrs = $this->compileAttrs($attrs);
-
             // Surface set config and add icon and attrs
             $cached = $this->getView()->render(
                 'Helpers/icons/' . $template,
@@ -218,7 +215,7 @@ class Icon extends AbstractHelper
                     $this->config['sets'][$set] ?? [],
                     [
                         'icon' => ($this->esc)($icon),
-                        'attrs' => $attrs,
+                        'attrs' => $this->compileAttrs($attrs),
                         'extra' => $attrs
                     ]
                 )
